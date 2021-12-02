a816150d36cfe391a3c885515faf8366b4e718c3
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-02 00:16:26 -0300

    Instanciar o Cliente: nome, email e dataNascimento

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,8 +1,11 @@
 package aplicacao;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entidades.Cliente;
 
 public class Programa {
 
@@ -13,6 +16,15 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+
+		System.out.println("Entre com os dados do Cliente:");
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("data Nascimento (DD/MM/YYYY): ");
+		Date dataNascimento = sdf.parse(sc.next());
+		Cliente cliente = new Cliente(nome, email, dataNascimento);
 		
 		
 		sc.close();
