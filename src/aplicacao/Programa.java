8400257345ce723c77497cb8f98a68c40cbeb382
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-02 00:05:42 -0300

    Primeiro

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,24 @@
+package aplicacao;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) thows ParseException{
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+		
+		
+
+	}
+
+}
